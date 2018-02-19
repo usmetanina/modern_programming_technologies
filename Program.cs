2bdd9b6459a1b178ed6345dcc92295a1c8b6ca02
usmetanina <usmetanina96@mail.ru>
2018-02-19 23:51:37 +0500

    add container

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Ninject;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject.Modules;
+
+namespace ioc
+{
+    public class ConsoleModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<ILogger>().To<ConsoleLogger>().InSingletonScope();
+            Bind<IDatabase>().To<Database>().InTransientScope();
+        }
+    }
+
+    public interface ILogger
+    {
+        void Log(string message);
+    }
+
+    class ConsoleLogger: ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public interface IDatabase
+    {
+        void Execute(string query, IKernel kernel);
+    }
+
+    class Database : IDatabase
+    {
+        public void Execute(string query, IKernel kernel)
+        {
+            kernel.Get<ILogger>().Log(query);
+        }
+    }
+
+    //public class Resolver
+    //{
+    //    IKernel ninjectKernel = new StandardKernel(new ConsoleModule());
+    //    public IKernel getKernel()
+    //    {
+    //        return ninjectKernel;
+    //    }
+    //}
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKernel ninjectKernel = new StandardKernel(new ConsoleModule());
+            for (int i=0; i<10; i++)
+            {
+                ninjectKernel.Get<IDatabase>().Execute(i.ToString(), ninjectKernel);
+            }
+        }
+    }
+}
